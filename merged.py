5177346ef30eb4bf7d041582e9648010f5521d83
p1r473 <subwayjared@gmail.com>
2024-03-28 00:38:28 -0400

    Update merged.py

--- merged.py
+++ merged.py
@@ -14,11 +14,6 @@ SHUTDOWN_THRESHOLD = 3  # Number of consecutive failures required for shutdown
 SLEEP_TIME = 60  # Time in seconds to wait between failure checks
 Loop =  False
 
-# Ensure only one instance of the script is running
-pid = str(os.getpid())
-pidfile = "/run/X1200.pid"
-
-# Define functions for reading voltage and capacity
 def readVoltage(bus):
     read = bus.read_word_data(address, 2)
     swapped = struct.unpack("<H", struct.pack(">H", read))[0]
@@ -31,6 +26,23 @@ def readCapacity(bus):
     capacity = swapped / 256
     return capacity
 
+def get_battery_status(voltage):
+    if 3.87 <= voltage <= 4.2:
+        return "Full"
+    elif 3.7 <= voltage < 3.87:
+        return "High"
+    elif 3.55 <= voltage < 3.7:
+        return "Medium"
+    elif 3.4 <= voltage < 3.55:
+        return "Low"
+    elif voltage < 3.4:
+        return "Critical"
+    else:
+        return "Unknown"
+
+# Ensure only one instance of the script is running
+pid = str(os.getpid())
+pidfile = "/run/X1200.pid"
 if os.path.isfile(pidfile):
     print("Script already running")
     exit(1)
@@ -39,11 +51,8 @@ else:
         f.write(pid)
 
 try:
-    # Initialize SMBus (I2C)
     bus = smbus.SMBus(1)
     address = 0x36
-    
-    # GPIO setup for AC power monitoring
     PLD_PIN = 6
     chip = gpiod.Chip('gpiochip4')
     pld_line = chip.get_line(PLD_PIN)
@@ -52,14 +61,12 @@ try:
     while True:
         failure_counter = 0
 
-        # Main check logic
         for _ in range(SHUTDOWN_THRESHOLD):
             ac_power_state = pld_line.get_value()
             voltage = readVoltage(bus)
+            battery_status = get_battery_status(voltage)
             capacity = readCapacity(bus)
-            # Print and log status
-            print(f"Voltage: {voltage:.2f}V, Capacity: {capacity:.2f}%, AC Power State: {'OK' if ac_power_state == 1 else 'FAIL'}","True")
-            # Check for low battery
+            print(f"Capacity: {capacity:.2f}% ({battery_status}), AC Power State: {'Plugged in' if ac_power_state == 1 else 'Unplugged'}, Voltage: {voltage:.2f}V")
             if capacity < 20:
                 print("Battery level critical.")
                 failure_counter += 1
@@ -70,14 +77,12 @@ try:
                 print("UPS is unplugged or AC power loss detected.")
                 failure_counter += 1
             else:
-                # If none of the conditions are met, it means everything is OK
                 failure_counter = 0
                 break
 
             if failure_counter < SHUTDOWN_THRESHOLD:
-                time.sleep(SLEEP_TIME)  # Wait for the specified sleep time before the next check
+                time.sleep(SLEEP_TIME) 
 
-        # Evaluate whether to shutdown
         if failure_counter >= SHUTDOWN_THRESHOLD:
             shutdown_reason = ""
             if capacity < 20:
@@ -91,11 +96,8 @@ try:
             print(shutdown_message)
             call("sudo nohup shutdown -h now", shell=True)
         else:
-            # If the script exits the loop without reaching the shutdown threshold,
-            # it means the conditions for a critical shutdown were not met in consecutive checks.
             #print("System operating within normal parameters. No action required.")
             if Loop:
-                # If the conditions for a shutdown are not met, wait before starting the next set of checks
                 time.sleep(SLEEP_TIME)
             else:
                 exit(0)
